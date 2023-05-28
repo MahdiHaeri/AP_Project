5d25fbc51d9d0a48bd4cd6c72c0467abe563e460
Mohammad Mahdi Haeri <mahdi@Mahdis-Laptop.local>
2023-05-28 20:11:13 +0330

    update userController, make test in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.example.controllers.UserController;
+
+import java.sql.SQLException;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            UserController userController = new UserController();
+            userController.createUser("2", "mobin", "zaki", "mobinzaki@gmail.com", "123456789", "123456", "Iran", new Date());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/org/example/controllers/UserController.java
+++ src/main/java/org/example/controllers/UserController.java
@@ -1,16 +1,19 @@
 package org.example.controllers;
 
+import org.example.data_access.UserDAO;
 import org.example.models.User;
 
+import java.sql.SQLException;
 import java.util.Date;
 
 public class UserController {
-    public UserController() {
-
+    private final UserDAO userDAO;
+    public UserController() throws SQLException {
+        userDAO = new UserDAO();
     }
 
-    public void createUser(String id, String firstName, String lastName, String email, String phoneNumber, String password, String country, Date birthday) {
+    public void createUser(String id, String firstName, String lastName, String email, String phoneNumber, String password, String country, Date birthday) throws SQLException {
         User user = new User(id, firstName, lastName, email, phoneNumber, password, country, birthday);
+        userDAO.saveUser(user);
     }
-
 }
