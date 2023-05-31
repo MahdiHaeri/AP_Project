096949ebcbe1b745946b1e33fa9712f7a7d91610
Mohammad Mahdi Haeri <mahdi@Mahdis-Laptop.local>
2023-05-31 20:55:21 +0330

    Add some user in server for test

--- src/main/java/org/example/server/Server.java
+++ src/main/java/org/example/server/Server.java
@@ -4,12 +4,25 @@ import com.sun.net.httpserver.HttpServer;
 import org.example.server.HttpHandlers.FollowHandler;
 import org.example.server.HttpHandlers.TweetHandler;
 import org.example.server.HttpHandlers.UserHandler;
+import org.example.server.controllers.UserController;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.sql.SQLException;
+import java.util.Date;
 
 public class Server {
     public static void main(String[] args) {
+//        try {
+//            UserController userController = new UserController();
+//            userController.createUser("mahdi", "mahdi", "haeri", "mahdihaerim@gmail.com", "123123123", "123", "Iran", new Date());
+//            userController.createUser("1", "ali", "haeri", "mahdihaerim@gmail.com", "123123123", "123", "Iran", new Date());
+//            userController.createUser("2", "sadegh", "haeri", "mahdihaerim@gmail.com", "123123123", "123", "Iran", new Date());
+//            userController.createUser("3", "javad", "haeri", "mahdihaerim@gmail.com", "123123123", "123", "Iran", new Date());
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//        System.exit(0);
         try {
             HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
             server.createContext("/users", new UserHandler());
