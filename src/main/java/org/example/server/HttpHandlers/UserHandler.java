1b6bf7c0516f8e5d49b093183974e02283d4e801
Mohammad Mahdi Haeri <mahdi@Mahdis-Laptop.local>
2023-05-31 21:13:24 +0330

    Complete Post /users

--- src/main/java/org/example/server/HttpHandlers/UserHandler.java
+++ src/main/java/org/example/server/HttpHandlers/UserHandler.java
@@ -55,7 +55,6 @@ public class UserHandler implements HttpHandler {
                 break;
             case "POST":
                 // Read the request body
-                System.out.println("here");
                 InputStream requestBody = exchange.getRequestBody();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(requestBody));
                 StringBuilder body = new StringBuilder();
@@ -68,16 +67,12 @@ public class UserHandler implements HttpHandler {
                 // Process the user creation based on the request body
                 String newUser = body.toString();
                 JSONObject jsonObject = new JSONObject(newUser);
-                System.out.println("here2");
-//                System.out.println(newUser);
-                System.out.println(jsonObject.getString("id"));
                 try {
-                    userController.createUser(jsonObject.getString("id"), jsonObject.getString("first_name"), jsonObject.getString("last_name"), jsonObject.getString("email"), jsonObject.getString("phoneNumber"), jsonObject.getString("password"), jsonObject.getString("country"), myConvert(jsonObject.getString("birthday")));
+                    userController.createUser(jsonObject.getString("id"), jsonObject.getString("firstName"), jsonObject.getString("lastName"), jsonObject.getString("email"), jsonObject.getString("phoneNumber"), jsonObject.getString("password"), jsonObject.getString("country"), new Date(jsonObject.getLong("birthday")));
                 } catch (SQLException e) {
                     throw new RuntimeException(e);
                 }
                 response = "this is done!";
-                System.out.println("should be done");
                 break;
             case "PUT":
                 response = "This is the response users Put";
