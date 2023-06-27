d2e30ccd67ea3c76f653802eb3ec85cfae83b121
MahdiHaeri <mahdihaerim@gmail.com>
2023-06-27 14:56:07 +0330

    Add Check user exist in Add Follow

--- src/main/java/com/example/server/controllers/FollowController.java
+++ src/main/java/com/example/server/controllers/FollowController.java
@@ -1,13 +1,17 @@
 package com.example.server.controllers;
 
 import com.example.server.data_access.FollowDAO;
+import com.example.server.data_access.UserDAO;
 import com.example.server.models.Follow;
+import com.example.server.models.User;
 
 import java.sql.SQLException;
 public class FollowController {
     private final FollowDAO followDAO;
+    private final UserDAO userDAO;
     public FollowController() throws SQLException {
         followDAO = new FollowDAO();
+        userDAO = new UserDAO();
     }
 
     public void createFollowTable() throws SQLException {
@@ -16,6 +20,7 @@ public class FollowController {
 
     public void saveFollow(String follower, String followed) throws SQLException {
         Follow follow = new Follow(follower, followed);
+        if (userDAO.getUser(follower) == null || userDAO.getUser(followed) == null) throw new SQLException("User does not exist");
         followDAO.saveFollow(follow);
     }
 
