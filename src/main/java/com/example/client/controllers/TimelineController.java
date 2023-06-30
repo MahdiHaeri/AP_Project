1add21a18bc5a2e5cffe2568ee7460e76b8aa3de
MahdiHaeri <mahdihaerim@gmail.com>
2023-06-30 13:29:08 +0330

    Add read tweets information from server Get /tweets

--- src/main/java/com/example/client/controllers/TimelineController.java
+++ src/main/java/com/example/client/controllers/TimelineController.java
@@ -1,12 +1,27 @@
 package com.example.client.controllers;
 
+import com.example.server.models.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.layout.VBox;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class TimelineController implements Initializable {
@@ -16,17 +31,55 @@ public class TimelineController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        int n = 10;
-        for (int i = 0; i < n; i++) {
-            try {
-//                TweetController tweetController = new TweetController();
-//                tweetController.setTweetText("Tweet " + i);
-                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/client/tweet.fxml"));
-                Node tweet = fxmlLoader.load();
-                tweetsVbox.getChildren().add(tweet);
-//                tweetsVbox.getChildren().add(tweetController.getTweet());
-            } catch (Exception e) {
-                e.printStackTrace();
+        // Make a GET request to the server
+        HttpURLConnection connection = null;
+        try {
+            URL apiUrl = new URL("http://localhost:8080/tweets");
+            connection = (HttpURLConnection) apiUrl.openConnection();
+            connection.setRequestMethod("GET");
+
+            // Check the response code
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                // Read the response
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String response = reader.readLine();
+                reader.close();
+
+                // Parse the JSON response
+                ObjectMapper objectMapper = new ObjectMapper();
+                JsonNode tweetsJson = objectMapper.readTree(response);
+
+                // Iterate over the tweets
+                for (JsonNode tweetJson : tweetsJson) {
+                    // Create an FXML loader and load the tweet FXML file
+                    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/client/tweet.fxml"));
+                    Parent tweetRoot = fxmlLoader.load();
+
+                    // Access the controller of the tweet FXML
+                    TweetController tweetController = fxmlLoader.getController();
+
+                    // Set the tweet information on the controller
+                    tweetController.setTweetText(tweetJson.get("text").asText());
+                    tweetController.setWriterNameLbl(tweetJson.get("writerId").asText());
+                    tweetController.setOwnerNameLbl(tweetJson.get("ownerId").asText());
+//                    tweetController.setTimestapLbl(tweetJson.get("timestamp").asText());
+                    tweetController.setQuoteLbl(tweetJson.get("quoteTweetId").asText());
+                    // ... set other information on the controller
+
+                    // Add the tweet to the tweet box
+                    tweetsVbox.getChildren().add(tweetRoot);
+                }
+            } else {
+                // Handle the error case when the server returns a non-OK response
+                System.out.println("Failed to retrieve tweets. Response code: " + responseCode);
+            }
+        } catch (IOException e) {
+            // Handle any IO exception that occurs during the request
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
             }
         }
     }
--- src/main/java/com/example/client/controllers/TweetController.java
+++ src/main/java/com/example/client/controllers/TweetController.java
@@ -56,6 +56,110 @@ public class TweetController implements Initializable {
     @FXML
     private Label writerUsernameLbl;
 
+//    public GNAvatarView getAvatarView() {
+//        return avatarView;
+//    }
+//
+//    public void setAvatarView(GNAvatarView avatarView) {
+//        this.avatarView = avatarView;
+//    }
+
+//    public ImageView getImageView() {
+//        return imageView;
+//    }
+//
+//    public void setImageView(ImageView imageView) {
+//        this.imageView = imageView;
+//    }
+
+//    public Button getLikeBtn() {
+//        return likeBtn;
+//    }
+//
+//    public void setLikeBtn(Button likeBtn) {
+//        this.likeBtn = likeBtn;
+//    }
+
+//    public MediaView getMeidaView() {
+//        return meidaView;
+//    }
+//
+//    public void setMeidaView(MediaView meidaView) {
+//        this.meidaView = meidaView;
+//    }
+
+    public String getOwnerNameLbl() {
+        return ownerNameLbl.getText();
+    }
+
+    public void setOwnerNameLbl(String text ) {
+        this.ownerNameLbl.setText(text); ;
+    }
+
+    public String getQuoteLbl() {
+        return quoteLbl.getText();
+    }
+
+    public void setQuoteLbl(String text) {
+        this.quoteLbl.setText(text);
+    }
+
+//    public Button getReplyBtn() {
+//        return replyBtn;
+//    }
+//
+//    public void setReplyBtn(Button replyBtn) {
+//        this.replyBtn = replyBtn;
+//    }
+//
+//    public Button getRetweetBtn() {
+//        return retweetBtn;
+//    }
+//
+//    public void setRetweetBtn(Button retweetBtn) {
+//        this.retweetBtn = retweetBtn;
+//    }
+//
+//    public Button getShareBtn() {
+//        return shareBtn;
+//    }
+//
+//    public void setShareBtn(Button shareBtn) {
+//        this.shareBtn = shareBtn;
+//    }
+
+    public String getTextMessageLbl() {
+        return textMessageLbl.getText();
+    }
+
+    public void setTextMessageLbl(String text) {
+        this.textMessageLbl.setText(text);
+    }
+
+    public String getTimestapLbl() {
+        return timestapLbl.getText();
+    }
+
+    public void setTimestapLbl(String text) {
+        this.timestapLbl.setText(text);
+    }
+
+    public String getWriterNameLbl() {
+        return writerNameLbl.getText();
+    }
+
+    public void setWriterNameLbl(String text) {
+        this.writerNameLbl.setText(text);
+    }
+
+    public String getWriterUsernameLbl() {
+        return writerUsernameLbl.getText();
+    }
+
+    public void setWriterUsernameLbl(String text) {
+        this.writerUsernameLbl.setText(text);
+    }
+
     @FXML
     void onLikeBtnAction(ActionEvent event) {
         System.out.println("Like button clicked");
