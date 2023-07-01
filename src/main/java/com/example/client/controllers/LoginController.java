b7910004f68e66b482dc792c2b0080abdf99216d
MahdiHaeri <mahdihaerim@gmail.com>
2023-07-01 22:42:43 +0330

    complete login page logic

--- src/main/java/com/example/client/controllers/LoginController.java
+++ src/main/java/com/example/client/controllers/LoginController.java
@@ -57,66 +57,50 @@ public class LoginController implements Initializable {
     void onLoginBtnAction(ActionEvent event) {
         // Make a GET request to the server
         HttpURLConnection connection = null;
-//        try {
-//            URL apiUrl = new URL("http://localhost:8080/login/" + usernameTf.getText() + "/" + passwordTf.getText());
-//            connection = (HttpURLConnection) apiUrl.openConnection();
-//            connection.setRequestMethod("GET");
-//
-//            // Check the response code
-//            int responseCode = connection.getResponseCode();
-//            if (responseCode == HttpURLConnection.HTTP_OK) {
-//                // Read the response
-//                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//                String response = reader.readLine();
-//                reader.close();
-//
-//                // Parse the JSON response
-//                ObjectMapper objectMapper = new ObjectMapper();
-//                JsonNode userJson = objectMapper.readTree(response);
-//
-//                // Set the labels
-////                usernameLbl.setText(userJson.get("id").asText());
-////                firstNameLbl.setText(userJson.get("firstName").asText());
-////                lastNameLbl.setText(userJson.get("lastName").asText());
-//////                bioLbl.setText(userJson.get("bio").asText());
-////                locationLbl.setText(userJson.get("country").asText());
-//
-//                Date date = new Date(userJson.get("createdAt").asLong());
-//                SimpleDateFormat sdf = new SimpleDateFormat("MMMM yyyy");
-//                String dateString = sdf.format(date);
-////                DateLbl.setText(dateString);
-
-//                followercountLbl.setText(userJson.get("followerCount").asText());
-//                followingCountLbl.setText(userJson.get("followingCount").asText());
-
-
-//            } else {
-//                // Handle the error case when the server returns a non-OK response
-//                System.out.println("Failed to retrieve tweets. Response code: " + responseCode);
-//            }
-//        } catch (IOException e) {
-//            // Handle any IO exception that occurs during the request
-//            e.printStackTrace();
-//        } finally {
-//            if (connection != null) {
-//                connection.disconnect();
-//            }
-//        }
-
         try {
-            // Load the FXML file for the SignUp page
-            Parent signupPage = FXMLLoader.load(getClass().getResource("/com/example/client/main.fxml"));
+            URL apiUrl = new URL("http://localhost:8080/login/" + usernameTf.getText() + "/" + passwordTf.getText());
+            connection = (HttpURLConnection) apiUrl.openConnection();
+            connection.setRequestMethod("GET");
 
-            // Create a new scene using the SignUp page
-            Scene signupScene = new Scene(signupPage);
+            // Check the response code
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                // Read the response
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String response = reader.readLine();
+                reader.close();
+                // save jwt
 
-            // Get the current stage (window) and set the new scene
-            Stage currentStage = (Stage) signupLink.getScene().getWindow();
-            currentStage.setScene(signupScene);
-            currentStage.setFullScreen(true);
+                try {
+                    // Load the FXML file for the SignUp page
+                    Parent signupPage = FXMLLoader.load(getClass().getResource("/com/example/client/main.fxml"));
+
+                    // Create a new scene using the SignUp page
+                    Scene signupScene = new Scene(signupPage);
+
+                    // Get the current stage (window) and set the new scene
+                    Stage currentStage = (Stage) signupLink.getScene().getWindow();
+                    currentStage.setScene(signupScene);
+                    currentStage.setFullScreen(true);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            } else {
+                // Handle the error case when the server returns a non-OK response
+                System.out.println("Failed to retrieve tweets. Response code: " + responseCode);
+                statusMessageLbl.setVisible(true);
+                statusMessageLbl.setText("Username or password is not valid");
+            }
         } catch (IOException e) {
+            // Handle any IO exception that occurs during the request
             e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
         }
+
     }
 
     @FXML
